c6221db13798b39fbf04bfb23a6ce7e037362cf2
MuzamilChouhan <muzamil.chouhaan@gmail.com>
2025-02-22 02:12:21 +0500

    commit

---
+++ src/index.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { makeServer } from "./server";
+import { BrowserRouter as Router } from "react-router-dom";
+import { DataProvider } from "./contexts/DataProvider.js";
+import { AuthProvider } from "./contexts/AuthProvider.js";
+import { UserProvider } from "./contexts/UserDataProvider.js";
+import { AddressProvider } from "./contexts/AddressProvider.js";
+
+// Call make Server
+makeServer();
+
+const container = document.getElementById("root");
+const root = createRoot(container);
+
+root.render(
+  <React.StrictMode>
+    <Router>
+      <AuthProvider>
+        <DataProvider>
+          <UserProvider>
+            <AddressProvider>
+              <App />
+            </AddressProvider>
+          </UserProvider>
+        </DataProvider>
+      </AuthProvider>
+    </Router>
+  </React.StrictMode>
+);
